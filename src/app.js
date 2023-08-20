c59a3aecb6207d14795d04ec90328f76a084b0dc
Crong <crong@i-on.net>
2023-08-20 19:30:19 +0900

    feat: webpack 을 이용한 FE 개발 환경 구성 하기

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import { sum } from './math.js'
+import './app.css'
+import webpackImage from './logo.png'
+
+window.addEventListener('DOMContentLoaded', () => {
+  sum(1, 2);
+  const el = document.querySelector('#app');
+  el.innerHTML = `
+    <h1>1+2=${sum(1, 2)}</h1>
+    <img src="${webpackImage}" alt='webpack'/>
+  `
+})
